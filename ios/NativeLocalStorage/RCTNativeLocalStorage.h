9268da65d03bf9b42217d455f699a2b4c7c71cc9
Nil Gandhi <nil.gandhi@kombee.com>
2024-12-18 16:02:02 +0530

    first commit

---
+++ ios/NativeLocalStorage/RCTNativeLocalStorage.h
@@ -0,0 +1,18 @@
+//
+//  RCTNativeLocalStorage.h
+//  TurboModuleExample
+//
+//  Created by iMac on 18/12/24.
+//
+
+#import <Foundation/Foundation.h>
+#import <NativeLocalStorageSpec/NativeLocalStorageSpec.h>
+
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RCTNativeLocalStorage : NSObject <NativeLocalStorageSpec>
+
+@end
+
+NS_ASSUME_NONNULL_END
